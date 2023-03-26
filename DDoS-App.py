14a1f1d4c43b7db7eca74cffe2c1f90bf81c45d0
Rrezeartaa <rrezearta.thaqi@student.uni-pr.edu>
2023-03-26 21:29:49 +0200

    Rounded the number of malicious packets percentage
    Also renamed some functions and variables.

--- DDoS-App.py
+++ DDoS-App.py
@@ -12,23 +12,22 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import accuracy_score
 
-
 app = Flask(__name__)
 
 #http://localhost:8000/
 
 @app.route('/')
-def detection():
+def attacks_detection():
         return render_template('landing-page.html')
 
-@app.route('/ddos_detection', methods=['POST'])
-def ddos_detection():
+@app.route('/ddos_mitc_detection', methods=['POST'])
+def ddos_mitc_detection():
+        
     # Get the inputs from the request
-    
     target_ip = request.form['ip']
     
     # Define the DDoS and MitC detection function
-    def detect_attack(pkt):
+    def detect_attacks(pkt):
         if IP in pkt:
             src_ip = pkt[IP].src
             dst_ip = pkt[IP].dst
@@ -41,50 +40,48 @@ def ddos_detection():
                     # SYN or RST flags set
                     label = "malicious"
                     df = pd.DataFrame([[time.time(), src_ip, dst_ip, src_port, dst_port, flags, label]], columns=['timestamp', 'src_ip', 'dst_ip', 'src_port', 'dst_port', 'flags', 'label'])
-                    df.to_csv('ddos_and_mitc_attack.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attack.csv'))
+                    df.to_csv('ddos_and_mitc_attacks.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attacks.csv'))
                 elif 'HTTP' in pkt and len(pkt['HTTP'].fields) > 50:
                     # HTTP request with a large number of headers - MITC attack
                     label = "malicious"
                     df = pd.DataFrame([[time.time(), src_ip, dst_ip, src_port, dst_port, len(pkt['HTTP'].fields)]], columns=['timestamp', 'src_ip', 'dst_ip', 'src_port', 'dst_port', 'num_headers', 'label'])
-                    df.to_csv('ddos_and_mitc_attack.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attack.csv'))
+                    df.to_csv('ddos_and_mitc_attacks.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attacks.csv'))
                 else:
                     label = "normal"
                     df = pd.DataFrame([[time.time(), src_ip, dst_ip, src_port, dst_port, flags, label]], columns=['timestamp', 'src_ip', 'dst_ip', 'src_port', 'dst_port', 'flags', 'label'])
-                    df.to_csv('ddos_and_mitc_attack.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attack.csv'))
+                    df.to_csv('ddos_and_mitc_attacks.csv', mode='a', index=False, header=not os.path.exists('ddos_and_mitc_attacks.csv'))
     
     # Start sniffing packets
     start_time = time.time()
     duration = 20 * 60  # 20 minutes in seconds
     while time.time() - start_time < duration:
-            sniff(prn=detect_attack, timeout=10)
+            sniff(prn=detect_attacks, timeout=10)
 
-##
-### Load the data from the CSV file
-    df = pd.read_csv('ddos_and_mitc_attack.csv')
+    # Load the data from the CSV file
+    df = pd.read_csv('ddos_and_mitc_attacks.csv')
 
-        # Define the features and labels
+    # Define the features and labels
     X = df[['timestamp', 'src_ip', 'dst_ip', 'src_port', 'dst_port', 'flags']]
     y = df['label']
 
-        # One-hot encode the IP addresses
+    # One-hot encode the IP addresses
     ip_encoder = OneHotEncoder(categories='auto', sparse=False, handle_unknown='ignore')
     ip_transformer = ColumnTransformer([('one_hot', ip_encoder, ['src_ip', 'dst_ip', 'flags'])], remainder='passthrough')
     X = ip_transformer.fit_transform(X)
 
-        # Scale the features
+    # Scale the features
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
 
-        # Train the SVM model
+    # Train the SVM model
     model = SVC()
     model.fit(X, y)
 
-        # Make a prediction
+    # Make a prediction
     predictions = model.predict(X)
-    accuracy = accuracy_score(y, predictions)
-    print("Accuracy:", accuracy)
-    percent_malicious = sum(predictions == 'malicious') / len(predictions) * 100
-    print("Percentage of malicious packets in 20 minutes: {:.2f}%".format(percent_malicious))
+    percent_malicious_packets = sum(predictions == 'malicious') / len(predictions) * 100
+
+    percent_malicious = "{:.2f}".format(percent_malicious_packets)
 
     return redirect(url_for('show_result', percent_malicious=percent_malicious))
 
